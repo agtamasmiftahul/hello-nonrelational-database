db3f667fb4f0d1e337db4a69715e86caa455e7be
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2018-01-30 10:49:34 +0700

    Create data schema and insert data

--- server.js
+++ server.js
@@ -6,3 +6,39 @@ db.on('error', console.log.bind(console, 'Connection error :'));
 db.once('open', () => {
   console.log('MongoDB connected');
 });
+
+let Schema = mongoose.Schema;
+
+let heroesSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: String,
+    required: true,
+    default: 0
+  },
+  abilities: {}
+}, {
+  versionKey: false
+});
+
+let Heroes = mongoose.model('Heroes', heroesSchema);
+
+let anne = new Heroes({
+  name: 'Anne',
+  age: 22,
+  abilities: {
+    style: 'pusher',
+    xp: 450,
+    type: 'channel'
+  }
+});
+
+// POST data (save,create,insert)
+anne.save(function(err) {
+  if (err) throw err;
+
+  console.log('Heroes saved successfully!');
+});
