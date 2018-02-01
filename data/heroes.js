c21f0b7f1ff04fcd7272054edf990d31c6d652b7
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2018-02-01 10:59:35 +0700

    Update hero values for dummy data

--- data/heroes.js
+++ data/heroes.js
@@ -3,27 +3,21 @@ db.getCollection('heroes').drop();
 heroes = [{
   name: 'Tom',
   age: 24,
-  abilites: {
-    style: 'jungler',
-    xp: 500,
-    type: 'spell'
-  }
+  style: 'jungler',
+  xp: 500,
+  type: 'spell'
 }, {
   name: 'Jerry',
   age: 30,
-  abilites: {
-    style: 'mid-lanner',
-    xp: 550,
-    type: 'physical'
-  }
+  style: 'mid-lanner',
+  xp: 550,
+  type: 'physical'
 }, {
   name: 'Drinn',
   age: 40,
-  abilities: {
-    style: 'tanker',
-    xp: 600,
-    type: 'physical'
-  }
+  style: 'tanker',
+  xp: 600,
+  type: 'physical'
 }]
 
 db.getCollection('heroes').insert(heroes);
